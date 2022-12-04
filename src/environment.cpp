c40fe781bfcb3249f21dc403ca83b7f2e2220050
Alejandro Sanchez <Sanchez.alef22@gmail.com>
2022-12-03 20:00:46 -0800

    changed view from XY to FPS for project submition

--- src/environment.cpp
+++ src/environment.cpp
@@ -254,7 +254,7 @@ int main (int argc, char** argv)
     std::cout << "starting enviroment" << std::endl;
 
     pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-    CameraAngle setAngle = XY;
+    CameraAngle setAngle = FPS;
     
     //pcl::PointCloud<pcl::PointXYZ>::Ptr  temp;
     initCamera(setAngle, viewer);
